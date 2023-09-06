ad464e8a5c571bf9cfdd0cf7b97670ba1d9fc028
Marco Alarcon <markinsh0@gmail.com>
2023-09-06 02:58:29 -0500

    Commit final proyecto pizza

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ var btnMenuOpen = document.getElementById("btnMenuOpen"),
     enlaces.addEventListener("click", function(){
         menuResponsive.style.transitionDelay = "0.5s";
         menuResponsive.classList.remove("active");
+        menuResponsive.style.transitionDelay = "0s";
     });
 
 //SLIDER DE PRODUCTOS
@@ -34,3 +35,69 @@ var contenedor = document.querySelector(".slider"),
     btnIzquierdo.addEventListener("click", function(){
         contenedor.scrollLeft -= contenedor.offsetWidth;
     });
+
+//VALIDACION DE FORMULARIO
+var formulario = document.getElementById("formulario");
+
+function validar(e){
+    var inputNombre = document.getElementById("nombre"),
+        inputEmail = document.getElementById("email"),
+        inputComents = document.getElementById("comentarios"),
+        alertSuccess = document.getElementById("alertSuccess"),
+        alertError = document.getElementById("alertError");
+
+    if(inputNombre.value == 0 || inputEmail.value == 0 || inputComents.value == 0) {
+        e.preventDefault();
+        alertError.classList.remove("hide");
+        alertError.classList.add("show");
+        setTimeout(function() {
+            alertError.classList.remove("show");
+            alertError.classList.add("hide");
+        }, 2000);
+    } else {
+        e.preventDefault();
+        alertSuccess.classList.remove("hide");
+        alertSuccess.classList.add("show");
+        setTimeout(function() {
+            alertSuccess.classList.remove("show");
+            alertSuccess.classList.add("hide");
+        }, 2000);
+        inputNombre.value = "";
+        inputEmail.value = "";
+        inputComents.value = "";
+    }
+}
+
+//Eventos del formulario
+formulario.addEventListener("submit", validar);
+
+//BOTON DE SCROLL TOP
+var btnTop = document.getElementById("btn-top"),
+    logo = document.getElementById("logo");
+//Detectamos scroll en nuestra pagina web
+window.addEventListener("scroll", function(){
+    var scroll = document.documentElement.scrollTop,
+        fullSize = document.documentElement.offsetHeight,
+        sizeVP = document.documentElement.clientHeight;
+
+    if (scroll > 100) {
+        btnTop.classList.add("show");
+    } else {
+        btnTop.classList.remove("show");
+    }
+
+    if (fullSize == (scroll+sizeVP)) {
+        btnTop.classList.add("scrollFinal");
+    } else {
+        btnTop.classList.remove("scrollFinal");
+    }
+});
+
+btnTop.addEventListener("click", function(){
+    window.scrollTo(0,0);
+});
+
+//DETECTAR EVENTO CLICK EN EL LOGO
+logo.addEventListener("click", function(){
+    window.scrollTo(0,0);
+});
